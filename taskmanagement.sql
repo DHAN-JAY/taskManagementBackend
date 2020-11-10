d7f833e956f69b46ee6cbdd6eb6d403ba48896ec
Dhananjay Kumar <dkp12cs12@gmail.com>
2020-11-10 13:38:11 +0530

    DB changes

--- taskmanagement.sql
+++ taskmanagement.sql
@@ -16,34 +16,68 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `accounts`
+-- Table structure for table `accounts`
 --
 
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (11,'ANKUR KUMAR','ankur@gmail.com','Admin','$2b$10$O..g/pSJzj9ZE.rr7oxe3ewyV1DdEuchuLQL6JkuFjUKECefWTIrq'),(12,'SHYAM KUMAR','shyam@gmail.com','Admin','$2b$10$B2T53QGYB7XxXeiavv1b3e7e7CdR01zs5FCVrM5xSqHJPJiTTbLCa'),(19,'dhananjay','dh12@gmail.com','developer','$2b$10$xPRhUG.c2FGbDo5lLIvdxeaRwbIHJlw4HUsxHQxxSOA/zZnwcydwO'),(20,'Dhanjay','dh1234@gmail.com','manager','$2b$10$AThECzgDF/u90RaWexAiF.ZZDj8Xvrz.BHBhZklHlX89KHp0mJVyK');
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userName` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` text NOT NULL,
+  `password` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `projects`
+-- Table structure for table `projects`
 --
 
-LOCK TABLES `projects` WRITE;
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,11,'Utiliko','Business automation1',3,'2020-11-09 10:58:04','2020-11-10 10:36:30',0),(3,11,'MilagroCorp','Retail automation',3,'2020-11-09 11:11:56','2020-11-09 11:30:59',0),(4,12,'Style','Test',20,'2020-11-10 09:56:40','2020-11-10 11:07:33',0),(5,12,'Testing','Tested',20,'2020-11-10 10:34:13','2020-11-10 11:09:01',1);
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `projectName` text NOT NULL,
+  `projectDesc` mediumtext,
+  `assignedManager` int NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tasks`
+-- Table structure for table `tasks`
 --
 
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (2,12,0,'Video Calling','Able to video call contacts',1,1,'2020-11-10 12:12:25','2020-11-10 12:35:28',0),(3,20,0,'test task','testing task',0,19,'2020-11-10 12:24:40','2020-11-10 12:24:40',0);
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `managerId` int NOT NULL,
+  `projectId` int NOT NULL,
+  `taskName` text NOT NULL,
+  `taskDesc` mediumtext,
+  `status` tinyint NOT NULL DEFAULT '0',
+  `assignedDeveloper` int NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isDeleted` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `managerId` (`managerId`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`managerId`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -54,4 +88,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-10 13:34:30
+-- Dump completed on 2020-11-10 13:37:41
